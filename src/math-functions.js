3ea976bf80a6275264ecc0e42e7ccfc43f328a0a
Alan Willoughby <satyadvaya@gmail.com>
2020-04-03 15:47:16 -0700

    second commit attempt on second function

--- src/math-functions.js
+++ src/math-functions.js
@@ -27,7 +27,8 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
-
+    const product = a * b;
+    return [product, `The product of ${a} and ${b} is ${product}.`];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
